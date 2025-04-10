d7431ba596de47567a42d69c97bd871fadcf2124
Sanchit <sanchitumrao@gmail.com>
2025-04-10 14:08:18 +0530

    Add files via upload

---
+++ 08_modules.py
@@ -0,0 +1,8 @@
+#create a module named mymodule.py and import it in this file
+import mymodule as m
+
+print(m.add(5, 3))  # Output: 8
+print(m.sub(5, 3))  # Output: 2
+print(m.mul(5, 3))  # Output: 15
+print(m.div(5, 3))  # Output: 1.6666666666666667
+print(m.power(5, 3))  # Output: 125
