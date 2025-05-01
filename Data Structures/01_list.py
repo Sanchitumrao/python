68942a8dab612ec166f94e56feedf76cb174c9e3
Sanchit <sanchitumrao@gmail.com>
2025-05-01 17:13:51 +0530

    Add files via upload

---
+++ Data Structures/01_list.py
@@ -0,0 +1,13 @@
+#lists in python
+#lists are used to store multiple items in a single variable
+#lists are created using square brackets []
+#lists are ordered, changeable and allow duplicate values
+#lists can contain different data types
+#lists can be nested (list inside a list)
+
+lst=["apple","orange",5,3.06,False,"aakash","rohan"]
+print(lst[0])
+lst[0]="grapes"#unlike strings lists are mutable(changable)
+print(lst[0])
+print(lst[1:4])
+
